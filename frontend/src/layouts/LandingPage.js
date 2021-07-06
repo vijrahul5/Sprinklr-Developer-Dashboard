89fb5842d05fbaf58dc60a5f2d756672dd37bbde
varunm0503 <varunm0503@gmail.com>
2021-07-06 20:34:07 +0530

    Update LandingPage.js

--- frontend/src/layouts/LandingPage.js
+++ frontend/src/layouts/LandingPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Navbar from "../components/Navbar/Navbar";
 
+//review-cycle-1: don't use class components
 export default class LandingPage extends Component {
     render() {
         return (
