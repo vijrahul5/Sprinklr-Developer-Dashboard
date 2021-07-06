14e73b9c9939a6f20bec68bba5232773ebc568f5
varunm0503 <varunm0503@gmail.com>
2021-07-06 20:57:54 +0530

    Update Notification.js

--- frontend/src/layouts/JiraDashboard/Notify/Notification.js
+++ frontend/src/layouts/JiraDashboard/Notify/Notification.js
@@ -1,7 +1,10 @@
 import React, { useEffect } from "react";
+  //review-cycle-1:read about pusher js and document it
 import Pusher from "pusher-js";
 import { NotificationManager } from "react-notifications";
 import axios from "axios";
+
+  //review-cycle-1: why is dotenv needed
 require("dotenv").config();
 
 const channelid = process.env.REACT_APP_CHANNEL_ID_JIRA;
@@ -12,11 +15,13 @@ const pusher = new Pusher(channelid, {
 
 const Notification = () => {
   useEffect(() => {
+      //review-cycle-1: don't use IIFE. create a function outside useEffect and call it here
     (async function configureConnection() {
       let response = await axios.get("/api/jira/webhookToken");
       if (response.data.status === "Success") {
         cid = response.data.webhookId;
       } else {
+          //review-cycle-1: dont use alert to show notification error. can 
         alert("Error to bind notification channel");
       }
     })();
@@ -25,6 +30,7 @@ const Notification = () => {
       NotificationManager.success(
         `${data.details.type}`,
         `${data.details.key}`,
+          //review-cycle-1: what is 100000000000? take it out in a constant 
         100000000000
       );
     });
