784fe5556be01ca23a5378c4f96361dae5499e4c
varunm0503 <varunm0503@gmail.com>
2021-07-06 20:19:34 +0530

    Update Dashboard.js

--- frontend/src/layouts/Dashboard.js
+++ frontend/src/layouts/Dashboard.js
@@ -6,6 +6,8 @@ import StandUp from "./StandUp";
 import React from "react";
 import JiraDashboard from "./JiraDashboard/JIRA/Jira";
 
+//review-cycle-1: don't keep a single level directory structure. break it down. for eg: components has dashboard directory and signin component file. then there is a dashboard directory which has an index file which exports dashboard component. and dashboard directory also has other components used internaly by dashboard component
+//review-cycle-1: if possible, try to integrate prettier and eslint in your repo. 
 export default function Dashboard() {
     return (
         <>
