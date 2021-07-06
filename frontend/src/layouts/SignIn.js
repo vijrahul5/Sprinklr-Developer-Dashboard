89551e5d90154c9b348c2d53d7a7389fd7337f49
varunm0503 <varunm0503@gmail.com>
2021-07-06 20:16:52 +0530

    Update SignIn.js

--- frontend/src/layouts/SignIn.js
+++ frontend/src/layouts/SignIn.js
@@ -3,8 +3,12 @@ import Navbar from "../components/Navbar/Navbar";
 import Auth from "../Auth";
 import GoogleLogin from "react-google-login";
 import dotenv from "dotenv";
+
+//review-cycle-1: what is dotenv?
 dotenv.config();
 
+//review-cycle-1: these components should be present under components structure and not under layout folder. Read https://blog.bitsrc.io/sharing-react-components-from-atoms-and-molecules-to-pages-2d0d722b1dba?gi=a31d933d0b8a
+//review-cycle-1: convert to functional component
 export default class SignIn extends Component {
     // Component for the sign in page
     async handleGoogleLoginSuccess({ tokenId }) {
@@ -23,10 +27,12 @@ export default class SignIn extends Component {
                     <div className="signin">
                         <h3>Sign In</h3>
                         <GoogleLogin
+                            //review-cycle-1: you don't need template string here
                             clientId={`${process.env.REACT_APP_CLIENT_ID}`}
                             buttonText="Continue With Google"
                             onSuccess={this.handleGoogleLoginSuccess}
                             onFailure={this.handleGoogleLoginFailiure}
+                            //review-cycle-1: can be written as cookiePolicy="single_host_origin". 
                             cookiePolicy={"single_host_origin"}
                         />
                     </div>
