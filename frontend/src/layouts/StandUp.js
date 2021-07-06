9db2b163eb36d269cd751fb1703cb4fe25e1d99c
varunm0503 <varunm0503@gmail.com>
2021-07-06 20:28:48 +0530

    Update StandUp.js

--- frontend/src/layouts/StandUp.js
+++ frontend/src/layouts/StandUp.js
@@ -7,6 +7,7 @@ import { Button} from "baseui/button";
 import { Textarea } from "baseui/textarea";
 import {SIZE} from "baseui/input";
 
+//review-cycle-1: this is a very big component. break it down into smaller components.
 function StandUp() {
     // Component for Stand Up message submission or editing
     const [value, setValue] = useState({
@@ -14,9 +15,11 @@ function StandUp() {
         question2: "",
         question3: "",
     });
+    
     const [loading, data, error] = useFetchEmployeeStandUp(setValue); // Fetches employee's stand up for the day
     const [addError, editError, addStandUp, editStandUp] = useUpdateStandUp(); // Provides functions for adding or deleting stand up
 
+    //review-cycle-1: is error a boolean?
     if (error !== false) {
         return <h1>{error}</h1>;
     }
@@ -30,6 +33,7 @@ function StandUp() {
     function handleSubmit(e) {
         // Event Listener for adding a standup
         e.preventDefault();
+        //review-cycle-1: why cant we read this data from state instead?
         const formData = new FormData(e.target);
         const data = Object.fromEntries(formData.entries());
         for (let key in data) {
@@ -38,6 +42,7 @@ function StandUp() {
                 return;
             }
         }
+        //review-cycle-1: do not manipulate dom directly. use state to empty inputs
         document.querySelectorAll(".standUpform input").forEach((element) => {
             element.value = "";
         });
@@ -55,6 +60,7 @@ function StandUp() {
                 return;
             }
         }
+        //review-cycle-1: same as above
         document.querySelectorAll(".standUpform input").forEach((element) => {
             element.value = "";
         });
@@ -62,12 +68,17 @@ function StandUp() {
     }
 
     if (loading) {
+        //review-cycle-1: we should create some tombstones instead if just rendering a loader
         return (
             <form className="standUpForm">
                 <Loader />
             </form>
         );
     } else {
+        //review-cycle-1: no need of else here. if branch has a return statement    
+        //review-cycle-1: a lot of duplicate code here for add and edit modes. take out common components.
+        //review-cycle-1: make separate components for add and edit mode. you can also use bridge pattern here 
+        //review-cycle-1: can we generalise this form a bit so that it is easy to added a new question in this form?
         return (
             <>
                 {data ? (
@@ -79,6 +90,7 @@ function StandUp() {
                                 value={value.question1}
                                 name="question1"
                                 className="form-control"
+                                //review-cycle-1: take out function in a variable here and check if we can wrap function with useCallback react hook
                                 onChange={(e) => {
                                     setValue({
                                         ...value,
@@ -177,6 +189,7 @@ function StandUp() {
                                 size={SIZE.mini}
                             />
                         </FormControl>
+                        //review-cycle-1: we can use onSubmit function here instead of using native form tag submit behaviour 
                         <Button type="submit" className="submit" size={SIZE.compact}>
                             Submit
                         </Button>
