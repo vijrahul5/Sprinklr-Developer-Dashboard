aa8db0a0bcc77b328b24188a47d37f5be068d20f
varunm0503 <varunm0503@gmail.com>
2021-07-06 20:35:56 +0530

    Update CustomRoutes.js

--- frontend/src/CustomRoutes.js
+++ frontend/src/CustomRoutes.js
@@ -5,6 +5,7 @@ import { useVerifyPublicRoute, useVerifyPrivateRoute } from "./Api";
 
 //Functions for providing protected frontend routing
 
+//review-cycle-1: everything looks same in both PublicRoute and PrivateRoute component. Just use a sinlgle component with type param
 export const PublicRoute = ({ component: Component, ...rest }) => { // Creates a public route which first verifies if a user is authenticated or not. If yes, then redirects to the dashboard only.
     const [loading, error] = useVerifyPublicRoute(true, false);
     if (error !== false) {
