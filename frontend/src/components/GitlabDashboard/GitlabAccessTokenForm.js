6d8b936ae5525e4c9b661f2eb2175da989b1b9a1
Rahul Vij <vijrahul5>
2021-07-11 20:41:40 +0530

    merged

--- frontend/src/components/GitlabDashboard/GitlabAccessTokenForm.js
+++ frontend/src/components/GitlabDashboard/GitlabAccessTokenForm.js
@@ -10,29 +10,40 @@ const GitlabAccessTokenForm = (props) => {
     };
     const submitToken = useCallback(() => {
         props.submitToken(accessToken);
-    }, []);
+    }, [accessToken, setAccessToken]);
     return (
         <>
-            <div>
-                <h1>Steps you should follow to Use Gitlab</h1>
-                <br></br>
-                <h3>1. Login to your gitlab Account</h3>
-                <h3>2. Select your gitlab icon and open preferences</h3>
-                <h3>3. Select Access Token in User Settings</h3>
-                <h3>4. Write token name in the input box</h3>
-                <h3>
+            <ul className="instruction">
+                <h2 className="instruction__item">
+                    1. Login to your gitlab Account
+                </h2>
+                <h2 className="instruction__item">
+                    2. Select your gitlab icon and open preferences
+                </h2>
+                <h2 className="instruction__item">
+                    3. Select Access Token in User Settings
+                </h2>
+                <h2 className="instruction__item">
+                    4. Write token name in the input box
+                </h2>
+                <h2 className="instruction__item">
                     5. Select scope as api and just click on create accessToken
-                </h3>
-                <h3>6. Now use this token to use our dashboard</h3>
-            </div>
-            <br></br>
+                </h2>
+                <h2 className="instruction__item">
+                    6. Now use this token to use our dashboard
+                </h2>
+            </ul>
             <Input
                 type="text"
+                value={accessToken}
                 placeholder="pleaseEnterAccesToken"
                 onChange={setToken}
             />
-            <br></br>
-            <Button onClick={submitToken}>SubmitToken</Button>
+            <div className="btn__container">
+                <Button onClick={submitToken} className="btn--auth">
+                    SubmitToken
+                </Button>
+            </div>
         </>
     );
 };
